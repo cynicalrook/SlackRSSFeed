c145ae96c4bde14d9b03d45039aeb3da9c0fa853
CynicalRook <cynicalrook@gmail.com>
2020-03-24 12:56:44 -0400

    d

--- rssdilbert.py
+++ rssdilbert.py
@@ -1,8 +1,12 @@
 import os
-from datetime import date, timedelta
+from datetime import date
+import configparser
 import requests
 from bs4 import BeautifulSoup as bs
+from slackclient import SlackClient
 
+#slack_channel = "CEKB88A1Y"
+slack_channel = "GDQ7JPD8U"
 
 BASE_URL = "https://dilbert.com/strip/"
 NEWEST_COMIC = date.today()
@@ -22,7 +26,6 @@ def get_comic_strip_url(start_date):
     full_url.append(BASE_URL+str(start_date))
     return full_url
 
-
 def get_image_comic_url(session, response):
     """
     Fetches the comic strip image source url based on the strip url
@@ -41,6 +44,9 @@ def download_dilbert(s, u):
         response = s.get(u)
         file.write(response.content)
 
+def post_to_slack(slack_client, comic):
+    slack_client.api_call("chat.postMessage", channel=slack_channel, text='Test', as_user = True)
+
 def download_engine(fcsd): #fcsd = first comic strip date
 
     """
@@ -53,12 +59,28 @@ def download_engine(fcsd): #fcsd = first comic strip date
         session = requests.Session()
         response = session.get(url)
         download_url = get_image_comic_url(session, response)
-        download_dilbert(session, download_url)
+#        download_dilbert(session, download_url)
+    return download_url
 
+def load_config(config_file, config_section):
+#    dir_path = os.path.dirname(os.path.relpath('config.ini'))
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+#    dir_path = os.path.abspath('.')
+    if os.path.isfile(dir_path + '/' + config_file):
+        config = configparser.ConfigParser()
+        config.read(config_file)
+        slack_token = config.get(config_section, 'token')
+    else:
+        slack_token = os.environ['token']
+    return slack_token
 
 def main():
-    download_engine(get_today())
-
+    config_file = 'config.ini'
+    config_section = 'dev'
+    slack_token = load_config(config_file, config_section)
+    slack_client = SlackClient(slack_token)    
+    comic = download_engine(get_today())
+    post_to_slack(slack_client,comic)
 
 if __name__ == '__main__':
     main()
